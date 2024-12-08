cb9a36ebc6bb1d69fc2c0339dc2f22c7606de63a
ablaberge <annika.laberge@icloud.com>
2024-12-07 21:18:49 -0800

    MongoDB connection block added to connect app with db using genericUser profile

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const express = require('express'); 
 const path = require('path');
 const app = express();              
-const port = 3000;                  
+const port = 3000;        
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://genericUser:5a1Vu2qe3f360L4F@spotifysongreccluster.fexy1.mongodb.net/?retryWrites=true&w=majority&appName=SpotifySongRecCluster";          
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server 
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 app.set('view engine', 'ejs')
@@ -13,3 +38,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+

c38138d5d4738492a6284400266fe4d416a0d19b
Gemma-B <gemma@bertain.net>
2024-12-08 18:06:12 -0800

    Got app.js queries running

--- app.js
+++ app.js
@@ -2,41 +2,36 @@ const express = require('express');
 const path = require('path');
 const app = express();              
 const port = 3000;        
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://genericUser:5a1Vu2qe3f360L4F@spotifysongreccluster.fexy1.mongodb.net/?retryWrites=true&w=majority&appName=SpotifySongRecCluster";          
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+const client = new MongoClient(uri);
 
-async function run() {
+
+// Replace the uri string with your connection string.
+async function run(songTitle, songArtist) {
   try {
-    // Connect the client to the server 
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const database = client.db('spotify_songs');
+    const tracks = database.collection('track');
+    // Query for a movie that has the title 'Back to the Future'
+    //const query = { track_name: "I Don't Care (with Justin Bieber) - Loud Luxury Remix" }, { track_artist: "Ed Sheeran"};
+    const songID = await tracks.find({
+      track_name: songTitle,
+      track_artist: songArtist,
+    },
+    {
+      track_id: 1
+    });
+    for await (const doc of songID) {
+      console.log(doc);
+  }
+    return songID.toArray();
+    
+
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
-run().catch(console.dir);
-
-app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
-app.set('view engine', 'ejs')
-
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
 
